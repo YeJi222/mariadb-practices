0542fc89ce12b6b9e973f764b54031ea233fb06b
YeJi222 <hhongyizi@gmail.com>
2023-09-07 23:25:28 +0900

    feat: delete sql statement file

--- jdbc-practices/deleteAlldataBookmall.sql
+++ jdbc-practices/deleteAlldataBookmall.sql
@@ -1,8 +1,14 @@
 -- delete all data from tables
-
 delete from order_book;
 delete from orders;
 delete from cart;
 delete from book;
 delete from category;
 delete from member;
+
+-- auto_increment 초기화
+alter table book auto_increment=1;
+alter table cart auto_increment=1;
+alter table category auto_increment=1;
+alter table member auto_increment=1;
+alter table orders auto_increment=1;
